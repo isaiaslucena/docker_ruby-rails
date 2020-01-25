55756b8f45e95fe3bbda05ab3ffb77cc65dd8218
Isaias Neto <isaiasneto@gmail.com>
2020-01-25 16:30:23 -0300

    set to clean pid server

--- run.sh
+++ run.sh
@@ -2,8 +2,9 @@
 
 if [[ -f "/FIRST_RUN" ]] ; then
 	rm -rf /FIRST_RUN
-    gem install bundler:2.0.2
+	gem install bundler:2.0.2
 	bundle install
 fi
 
+rm -rf /app/tmp/pids/server.pid
 rails s -b 0.0.0.0 -p 8080
