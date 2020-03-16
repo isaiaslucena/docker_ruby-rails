17944991785e9517eda4654ab145a85305f6aecd
Isaias Neto <isaiasneto@gmail.com>
2020-03-16 19:05:27 -0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,9 +3,9 @@
 if [[ -f "/FIRST_RUN" ]] ; then
 	rm -rf /FIRST_RUN
 	gem install bundler:2.0.2
-	bundle install
 fi
 
 rm -rf /app/tmp/pids/server.pid
+bundle install
 source .env
 rails s -b 0.0.0.0 -p 8080
